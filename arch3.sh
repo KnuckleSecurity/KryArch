089b3445ae42d3ecf62ce626f1808e1a67a458b4
krygeNNN <burak.baris1@hotmail.com>
2021-10-19 01:31:49 +0100

    Initial Commit

--- arch3.sh
+++ arch3.sh
@@ -8,8 +8,7 @@
 #Website:krygeNNN.github.io
 (
     pacman -Sy grub efibootmgr dosfstools os-prober mtools
-    rm -rf /boot/*
     mkdir /boot/efi
     mount /dev/sda1 /boot/efi
-    grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
+    grub-install --target=x86_64-efi --bootloader-id=grub_uefi --efi-directory=/boot/efi --recheck
 ) | tee arch3-logs.txt
