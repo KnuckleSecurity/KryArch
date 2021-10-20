13819cdc1d6d5124644a432911dc15d2fa061c48
krygeNNN <burak.baris1@hotmail.com>
2021-10-20 03:41:08 +0100

    Initial Commit

--- kryarch-1.sh
+++ kryarch-1.sh
@@ -76,8 +76,8 @@ genfstab -U /mnt >> /mnt/etc/fstab
 mv ~/kryarch /mnt/root
 #Entering arch-chroot environment
 ) 1>2 2>/dev/null
+kill $(cat /tmp/running) 
 clear
 echo "----------------------------------------------------------------------------------------------"
 echo -e "* Entered to the arch-chroot environment. To proceed with the installiation follow those steps.\n1- cd root/kryarch\n2- bash kryarch-2.sh"
 echo "----------------------------------------------------------------------------------------------"
-arch-chroot /mnt
