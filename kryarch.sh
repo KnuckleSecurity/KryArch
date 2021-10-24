d3810d7b3810b09537fecf6e7dec7ca26633e49d
krygeNNN <burak.baris1@hotmail.com>
2021-10-24 09:58:02 +0100

    Initial Commit

--- kryarch.sh
+++ kryarch.sh
@@ -1,6 +1,10 @@
 bash part1.sh
-arch-chroot /mnt /root/KryArch/part2.sh
-source /mnt/root/KryArch/envs.conf
-if [[ $plasma == "true" ]]; then
-    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/KryArch/part3.sh
+if test -f flag_done; then
+    rm flag_done
+    arch-chroot /mnt /root/KryArch/part2.sh
+    source /mnt/root/KryArch/envs.conf
+    if [[ $plasma == "true" ]]; then
+        arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/KryArch/part3.sh
+    fi
 fi
+
