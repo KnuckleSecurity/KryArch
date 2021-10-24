0ee83ce61db8524df64622562971405b3a3a9e30
krygeNNN <burak.baris1@hotmail.com>
2021-10-24 07:18:59 +0100

    Initial Commit

--- part3.sh
+++ part3.sh
@@ -3,14 +3,14 @@ git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si
 yay -S terminator firefox zsh fish konsave
+konsave -i misc/kryarch-one.knsv
+konsave -a kryarch-one
 cp misc/config ~/.config/terminator/config
 cp misc/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
 cp finland_forest_lake-wallpaper-1920x1080.jpg ~/Pictures
 cp finland_forest_lake-wallpaper-1920x1080.jpg /usr/share/sddm/themes/Nordic/assets/bg.jpg
 cp finland_forest_lake-wallpaper-1920x1080.jpg /usr/share/wallpapers/Next/contents/images/1920x1080.png
 cp misc/image2vector.svg ~/Pictures
-konsave -i misc/kryarch-one.knsv
-konsave -a kryarch-one
 
 sudo cat <<EOF >/etc/sddm.conf
 [Theme]
