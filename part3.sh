a747b48af105a7ddce312f8a4d51bf78ceec097a
krygeNNN <burak.baris1@hotmail.com>
2021-10-24 07:03:32 +0100

    Initial Commit

--- part3.sh
+++ part3.sh
@@ -2,9 +2,11 @@ sudo pacman -Sy git
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si
-yay -S terminator firefox zsh fish
+yay -S terminator firefox zsh fish konsave
 cp misc/config ~/.config/terminator/config
 cp misc/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
 cp finland_forest_lake-wallpaper-1920x1080.jpg ~/Pictures
 cp finland_forest_lake-wallpaper-1920x1080.jpg /usr/share/wallpapers/Next/contents/images/1920x1080.png
 cp image2vector.svg ~/Picturs
+konsave -i misc/kryarch-one.knsv
+konsave -a kryarch-one
